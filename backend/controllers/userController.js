848f332e18da2436285e533e500b946bf2976b77
johnsegs <131019313+johnolawoyin14@users.noreply.github.com>
2023-10-20 00:34:16 +0100

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const jwt=require("jsonwebtoken")
 
 
 const createToken=(_id)=>{
- return jwt.sign({_id},process.env.SECRET,{expiresIn:"3d"})  
+ return jwt.sign({_id},"johnolawoyinisaboyfromfederalunioftechakure",{expiresIn:"3d"})  
 }
 
 const loginUser=async(req,res)=>{
